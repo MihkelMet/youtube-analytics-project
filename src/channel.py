491479b4362abf30343d89768d212af15eac63b3
Mihkel Metsa <mihkel992@gmail.com>
2023-11-13 05:48:15 -0800

    channel.py adde classmethod + method

--- src/channel.py
+++ src/channel.py
@@ -8,14 +8,39 @@ class Channel:
     """Класс для ютуб-канала"""
     load_dotenv()
     api_key: str = os.getenv('YT_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        channel = self.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = channel['items'][0]['snippet']['title']
+        self.description = channel['items'][0]['snippet']['description']
+        self.url = f'https://www.youtube.com/channel/{channel["items"][0]["id"]}'
+        self.subscribe_count = channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.view_count = channel['items'][0]['statistics']['viewCount']
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=cls.api_key)
+
+    def to_json(self, filename):
+        json_dict = {
+            'channel_id': self.channel_id,
+            'title': self.description,
+            'description': self.description,
+            'url': self.url,
+            'subscriberCount': self.subscribe_count,
+            'videoCount': self.video_count,
+            'viewCount': self.view_count
+        }
+        with open(filename, 'w') as file:
+            json.dump(json_dict, file)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
 
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        channel = self.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        to_json = json.dumps(channel, indent=2, ensure_ascii=False)
+        print(to_json)
+        return to_json
